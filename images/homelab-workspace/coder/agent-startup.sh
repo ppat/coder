52f337cf1a0d64b4402b21dc40a5d213b9150c5a
Peter Pathirana <peter@latent.io>
2024-06-29 16:49:44 -0400

    fix: various image updates (#49)

--- images/homelab-workspace/coder/agent-startup.sh
+++ images/homelab-workspace/coder/agent-startup.sh
@@ -9,19 +9,26 @@ initialize_shell_config() {
       cp $skel $target
     fi
   done
-
+  if [[ ! -f ${HOME}/.local/bin/starship ]]; then
+    echo "Starship binary not found, installing..."
+    curl -sS https://starship.rs/install.sh | sh -s - --bin-dir $HOME/.local/bin -y
+  else
+    echo "Starship binary already exists, skipping install."
+  fi
   if [[ ! -f ${HOME}/.bashrc.optional ]]; then
     {
       # shellcheck disable=SC2016
       echo 'eval "$($FNM_ROOT/fnm env --shell bash --use-on-cd --fnm-dir $FNM_ROOT)"'
       # shellcheck disable=SC2016
-      echo 'eval "$(starship init bash)"'
+      echo 'eval "$(${HOME}/.local/bin/starship init bash)"'
     } >> ${HOME}/.bashrc.optional
   fi
 }
 
 main() {
+  echo "agent-startup-script: Starting..."
   initialize_shell_config
+  echo "agent-startup-script: Done."
 }
 
 main
