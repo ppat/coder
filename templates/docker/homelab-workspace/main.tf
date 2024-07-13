f1348aa034e60c87b5124f0bdf695dce79a9a841
Peter Pathirana <peter@latent.io>
2024-07-13 17:29:55 +0000

    feat: replace systemd with supervisord in coder image

--- templates/docker/homelab-workspace/main.tf
+++ templates/docker/homelab-workspace/main.tf
@@ -122,10 +122,13 @@ resource "docker_image" "workspace_image" {
 locals {
   standard_init_script   = replace(coder_agent.main.init_script, "/localhost|127\\.0\\.0\\.1/", "host.docker.internal")
   supervised_init_script = <<EOF
+    /opt/coder/bin/entrypoint-prepare.sh --username ${local.username};
+    echo "sudo -u ${local.username} --preserve-env=CODER_AGENT_TOKEN /bin/bash -- ${local.standard_init_script}" > /tmp/coder-agent-wrapper.sh;
+    chmod 700 /tmp/coder-agent-wrapper.sh;
     exec /usr/bin/supervisord
     EOF
 
-  agent_init_script = (local.test_mode) ? standard_init_script : supervised_init_script
+  agent_init_script = (local.test_mode) ? local.standard_init_script : local.supervised_init_script
 }
 
 resource "docker_container" "workspace" {
