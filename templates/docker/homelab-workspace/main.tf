06b7f9eecb35adfcb4087b3798e0e7a1614303c7
Peter Pathirana <peter@latent.io>
2024-07-19 17:15:14 -0400

    test: use base ubuntu:noble image for testing template (#109)

--- templates/docker/homelab-workspace/main.tf
+++ templates/docker/homelab-workspace/main.tf
@@ -1,116 +1,11 @@
-terraform {
-  required_version = ">= 1.8"
-
-  required_providers {
-    coder = {
-      source  = "coder/coder"
-      version = "~> 1.0.0"
-    }
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "~> 3.0.2"
-    }
-  }
-}
-
-provider "docker" {
-  host = "unix:///var/run/docker.sock"
-}
-
 data "coder_workspace" "me" {
 }
 
 data "coder_workspace_owner" "me" {
 }
 
-variable "workspace_image" {
-  type = string
-}
-
 locals {
-  test_mode = (var.workspace_image == "simple-workspace:latest")
-  username  = local.test_mode ? "coder" : data.coder_workspace_owner.me.name
-
-  container_volume_mounts = {
-    "home"   = "/home/${local.username}",
-    "docker" = "/var/lib/docker"
-  }
-  bind_mount_host_paths = {
-    "home"   = "/srv/workspaces/${local.username}",
-    "docker" = "/srv/workspaces/${local.username}-${data.coder_workspace.me.name}-docker"
-  }
-
-  # create docker volumes in test_mode
-  docker_volumes = local.test_mode ? ["home", "docker"] : []
-  # create bind mounts otherwise
-  bind_mounts = local.test_mode ? [] : ["home", "docker"]
-}
-
-
-resource "coder_agent" "main" {
-  arch                    = "amd64"
-  os                      = "linux"
-  startup_script          = local.test_mode ? "/bin/bash --noprofile --norc" : "/bin/bash --noprofile --norc /opt/coder/bin/agent-startup.sh"
-  startup_script_behavior = "blocking"
-
-  metadata {
-    display_name = "CPU Usage"
-    key          = "0_cpu_usage"
-    script       = "coder stat cpu"
-    interval     = 60
-    timeout      = 1
-  }
-
-  metadata {
-    display_name = "Memory Usage"
-    key          = "1_mem_usage"
-    script       = "coder stat mem --prefix Gi"
-    interval     = 60
-    timeout      = 1
-  }
-
-  metadata {
-    display_name = "Home Disk"
-    key          = "3_home_disk"
-    script       = "coder stat disk --path $${HOME}"
-    interval     = 60
-    timeout      = 1
-  }
-
-  metadata {
-    display_name = "CPU Usage (Host)"
-    key          = "4_cpu_usage_host"
-    script       = "coder stat cpu --host"
-    interval     = 60
-    timeout      = 1
-  }
-
-  metadata {
-    display_name = "Memory Usage (Host)"
-    key          = "5_mem_usage_host"
-    script       = "coder stat mem --host"
-    interval     = 60
-    timeout      = 1
-  }
-
-  metadata {
-    display_name = "Load Average"
-    key          = "load"
-    script       = <<EOT
-        awk '{print $1,$2,$3}' /proc/loadavg
-    EOT
-    interval     = 60
-    timeout      = 1
-  }
-}
-
-resource "docker_volume" "volume" {
-  for_each = toset(local.docker_volumes)
-
-  name = "coder-${data.coder_workspace.me.id}-${each.key}"
-  lifecycle {
-    ignore_changes = all
-  }
+  username = var.test_mode ? "coder" : data.coder_workspace_owner.me.name
 }
 
 resource "docker_image" "workspace_image" {
@@ -123,18 +18,48 @@ locals {
   standard_init_script = replace(coder_agent.main.init_script, "/localhost|127\\.0\\.0\\.1/", "host.docker.internal")
   entrypoint_script    = <<EOF
 echo "Running entrypoint script..."
+exec 2>&1
+echo "---------------------------------------------------------------------------------"
+echo "Writing coder agent init script to file..."
 cat > /tmp/coder-agent-init-script.sh <<'EOT'
 ${local.standard_init_script}
 EOT
 chmod 755 /tmp/coder-agent-init-script.sh
+echo "---------------------------------------------------------------------------------"
+echo "Checking minimum requirements for coder agent..."
+if (command -v curl && command -v sudo && command -v useradd) > /dev/null; then
+  echo "Minimum requirements for running coder agent is met."
+else
+  echo "Installing minimum required software for running coder agent..."
+  apt-get update
+  DEBIAN_FRONTEND="noninteractive" apt-get install -yq --no-install-recommends curl sudo adduser
+fi
+echo "---------------------------------------------------------------------------------"
+if grep coder /etc/passwd > /dev/null; then
+  echo "Modifying user: coder -> ${local.username}..."
+  usermod --home /home/${local.username} --shell /bin/bash --login $username coder
+else
+  echo "Creating user - ${local.username}..."
+  useradd --groups sudo --home-dir /home/${local.username} --shell /bin/bash ${local.username}
+fi
+# allow coder user to sudo to so that they can run any system actions (such as using apt-get) within their workspace container.
+echo "Enabling ${local.username} to sudo"
+echo "${local.username} ALL=(ALL) NOPASSWD:ALL" >/etc/sudoers.d/${local.username}
+chmod 0440 /etc/sudoers.d/${local.username}
+echo "---------------------------------------------------------------------------------"
+echo "Creating directories and updating directory permissions..."
+mkdir -p /home/${local.username}/.log/
+chown ${local.username} /home/${local.username}
+chown ${local.username} /home/${local.username}/.log/
+echo "---------------------------------------------------------------------------------"
+echo "Preparation complete. Starting coder agent..."
 if [[ "$ENTRYPOINT_MODE" == "SUPERVISED" ]]; then
-  echo "Running in supervised mode..."
-  /opt/coder/bin/entrypoint-prepare.sh --username ${local.username}
+  echo "Running agent in supervised mode..."
   echo -e "#!/bin/bash\nsudo -u ${local.username} --preserve-env=CODER_AGENT_TOKEN /bin/bash /tmp/coder-agent-init-script.sh" > /tmp/coder-agent-wrapper.sh
   chmod 755 /tmp/coder-agent-wrapper.sh
   exec /usr/bin/supervisord -c /etc/supervisord.conf
 else
-  echo "Running in unsupervised mode..."
+  echo "Running agent in unsupervised mode..."
   sudo -u ${local.username} --preserve-env=CODER_AGENT_TOKEN /bin/bash /tmp/coder-agent-init-script.sh
 fi
 EOF
@@ -144,7 +69,7 @@ resource "docker_container" "workspace" {
   count = data.coder_workspace.me.start_count
 
   image    = docker_image.workspace_image.image_id
-  name     = local.test_mode ? data.coder_workspace.me.name : "${lower(local.username)}-${lower(data.coder_workspace.me.name)}"
+  name     = var.test_mode ? data.coder_workspace.me.name : "${lower(local.username)}-${lower(data.coder_workspace.me.name)}"
   hostname = data.coder_workspace.me.name
   runtime  = "sysbox-runc"
   user     = "0:0"
@@ -153,7 +78,7 @@ resource "docker_container" "workspace" {
 
   env = [
     "CODER_AGENT_TOKEN=${coder_agent.main.token}",
-    "ENTRYPOINT_MODE=${local.test_mode ? "UNSUPERVISED" : "SUPERVISED"}"
+    "ENTRYPOINT_MODE=${var.test_mode ? "UNSUPERVISED" : "SUPERVISED"}"
   ]
   host {
     host = "host.docker.internal"
---
+++ templates/docker/homelab-workspace/terraform.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">= 1.8"
+
+  required_providers {
+    coder = {
+      source  = "coder/coder"
+      version = "~> 1.0.0"
+    }
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.2"
+    }
+  }
+}
+
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
