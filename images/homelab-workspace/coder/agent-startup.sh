2256e799eea8f457d51d29ae1eda53f30f432f21
Peter Pathirana <peter@latent.io>
2024-12-11 03:07:43 -0500

    refactor: move init-scripts from coder image to dotfiles (#350)

--- images/homelab-workspace/coder/agent-startup.sh
+++ images/homelab-workspace/coder/agent-startup.sh
@@ -1,34 +1,22 @@
 #!/bin/bash
 set -eo pipefail
 
-initialize_shell_config() {
-  # shellcheck disable=SC2044
-  for skel in $(find /etc/skel/ -type f); do
-    skel_type="$(basename ${skel})"
-    target="${HOME}/${skel_type}"
-    if [[ ! -f "$target" ]]; then
-      echo "${skel_type}: Copying from $skel to $target..."
-      cp $skel $target
-    else
-      echo "${skel_type}: already exists as $target."
-    fi
-  done
-}
 
-create_log_file() {
-  local prefix="$1"
-  local log="$HOME/.log/${prefix}.$(date +%Y%m%d_%H%M%S).log"
-  sudo touch $log
-  sudo chown $USER:coder $log
-  echo $log
-}
+DOTFILES_REPOSITORY="git@github.com:ppat/dotfiles.git"
 
-install_starship() {
-  if [[ ! -f $home_bin_dir/starship ]]; then
-    echo "Starship binary not found, installing..."
-    curl -sS https://starship.rs/install.sh | sh -s - --bin-dir $home_bin_dir -y
+fetch_dotfiles() {
+  if [[ ! -d $HOME/code/dotfiles ]]; then
+    echo "Cloning dotfiles repository..."
+    cd $HOME/code
+    git clone $DOTFILES_REPOSITORY 2>&1 | sed -E 's/^(.*)/    \1/g'
+  elif [[ -z "$(git status -s --porcelain)" ]]; then
+    echo "Updating dotfiles repository..."
+    cd $HOME/code/dotfiles
+    git fetch --prune 2>&1 | sed -E 's/^(.*)/    \1/g'
+    git pull origin master --rebase 2>&1 | sed -E 's/^(.*)/    \1/g'
   else
-    echo "Starship binary already exists, skipping install."
+    echo "Uncommitted changes preset in ~/code/dotfiles... cannot update!" >&2
+    exit 1
   fi
 }
 
@@ -40,20 +28,14 @@ main() {
   source /etc/environment
   set +o allexport
   echo "--------------------------------------------------------------------------------------"
-  echo "Initialize shell configuration from /etc/skel..."
-  initialize_shell_config | sed -E 's/^(.*)/    \1/g'
-  echo "--------------------------------------------------------------------------------------"
-  echo "Creating $HOME/.local/bin..."
-  local home_bin_dir="${HOME}/.local/bin"
-  mkdir -p $home_bin_dir
-  echo "--------------------------------------------------------------------------------------"
-  echo "Installing starship..."
-  install_starship | sed -E 's/^(.*)/    \1/g'
-  echo "--------------------------------------------------------------------------------------"
-  BACKGROUND_INIT_LOG="$(create_log_file "init-background")"
-  echo "Starting background initialization for long running tasks..."
-  stdbuf -oL nohup $(dirname ${0})/init-background.sh &> ${BACKGROUND_INIT_LOG} &
-  echo "See log: ${BACKGROUND_INIT_LOG}"
+  echo "Fetching dotfiles..."
+  if fetch_dotfiles | sed -E 's/^(.*)/    \1/g'; then
+    echo "Installing dotfiles..."
+    cd $HOME/code/dotfiles
+    ./install.sh | sed -E 's/^(.*)/    \1/g'
+  else
+    echo "Skipping dotfiles install as fetching dotfiles failed!" >&2
+  fi
   echo "--------------------------------------------------------------------------------------"
   echo "Done."
 }
