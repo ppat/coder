5d303e209df4130838fec02448a2026a723d590e
Peter Pathirana <peter@latent.io>
2026-09-17 01:08:45 +0000

    fix(template): support explicit in-cluster authentication

--- templates/kubernetes/homelab-workspace/terraform.tf
+++ templates/kubernetes/homelab-workspace/terraform.tf
@@ -17,5 +17,5 @@ provider "coder" {
 }
 
 provider "kubernetes" {
-  config_path = var.kubernetes_config_path != "" ? var.kubernetes_config_path : null
+  config_path = var.kubernetes_config_path != "in-cluster" ? var.kubernetes_config_path : null
 }
